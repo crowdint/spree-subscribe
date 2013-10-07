576de82b43b1e75790d616f0515550cd094450cc
Fernando Cuauhtemoc Barajas Chavez <fernando.barajas@crowdint.com>
2013-10-07 13:24:35 -0500

    [#58365930] Bugfix: subscription reminder
    -Two subscription reminder emails are sent when the
    subscription is ready to be reordered.

--- spec/tasks/reorders_rake_spec.rb
+++ spec/tasks/reorders_rake_spec.rb
@@ -3,18 +3,34 @@ require 'spec_helper'
 describe 'spree_subscribe:reorders:create' do
   include_context 'rake'
 
-  before do
-    FactoryGirl.create :subscription_for_reorder
-    FactoryGirl.create :subscription_for_reorder, reorder_on: 1.day.ago
+  let!(:today_subscription) do
+    FactoryGirl.create :subscription_for_reorder,
+      state:            'active',
+      times:            1,
+      time_unit:        1,
+      billing_address:  FactoryGirl.create(:address),
+      shipping_address: FactoryGirl.create(:address)
+  end
+  let!(:yesterday_subscription) do
+    FactoryGirl.create :subscription_for_reorder, reorder_on: 1.day.ago,
+      state:            'active',
+      times:            1,
+      time_unit:        1,
+      billing_address:  FactoryGirl.create(:address),
+      shipping_address: FactoryGirl.create(:address)
   end
 
-  context 'When exists active subscription' do
+  context 'When active subscription exist' do
     before do
+      Spree::Subscription.any_instance.stub(:complete_reorder).and_return true
       subject.invoke
+      today_subscription.reload
+      yesterday_subscription.reload
     end
 
-    it 'should reorder all' do
-      Spree::Subscription.all.each{ |s| s.new_order }
+    it 'reorders all' do
+      expect(today_subscription.reorder_on).to eql(Date.today + 1.day)
+      expect(yesterday_subscription.reorder_on).to eql(Date.today)
     end
   end
 end
